77bed10189cf2e0f9677e42c64840540af390ad1
Peter Rucker <peter@immunoconcepts.com>
2022-06-01 09:52:39 -0700

    Original Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from src.word import Word
+import enchant
+from nltk.corpus import words
+import itertools
+wordle = Word()
+wordle.grey = input("Input grey letters, or press enter to skip")
+
+for object in wordle.letterlist:
+    green = input(f"For position {object.position}, Input green letter, or press enter to skip: ")
+    if green != "":
+        object.letterlist = [green]
+        print(object.letterlist)
+        continue
+    yellows = input(f"For position {object.position}, Input yellow letters, or press enter to skip: ")
+    [object.letterlist.remove(x) for x in yellows]
+    [object.letterlist.remove(x) for x in wordle.grey]
+    wordle.required += yellows
+    print(object.letterlist)
+dictionary = enchant.Dict("en_US")
+generator = itertools.product(wordle.l1.letterlist,wordle.l2.letterlist,wordle.l3.letterlist,wordle.l4.letterlist,wordle.l5.letterlist)
+for element in generator:
+    if dictionary.check("".join(element)):
+        if all(x in element for x in wordle.required):
+            print("".join(element))
---
+++ src/word.py
@@ -0,0 +1,24 @@
+import string
+class Word(object):
+    def __init__(self) -> None:
+        super().__init__()
+        self.l1 = Letter(1)
+        self.l2 = Letter(2)
+        self.l3 = Letter(3)
+        self.l4 = Letter(4)
+        self.l5 = Letter(5)
+        self.letterlist = [self.l1,self.l2,self.l3,self.l4,self.l5]
+        self.grey = []
+        self.required = []
+
+    def __str__(self):
+        print([(str(x)) for x in self.letterlist])
+        return f"Banned letters = {self.grey}"
+
+class Letter(object):
+    def __init__(self,position) -> None:
+        super().__init__()
+        self.position = position
+        self.letterlist = [x for x in string.ascii_lowercase]
+    def __str__(self):
+        return f"Letter in position {self.position}. Available letters = {self.letterlist}\n"
